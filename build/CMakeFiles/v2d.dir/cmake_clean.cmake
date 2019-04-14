89a658f57966f555ac6894768ef1e9406b2c7f32
yuqli <greenstone1564@gmail.com>
2019-04-14 16:16:41 -0400

    add example

---
+++ build/CMakeFiles/v2d.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/v2d.dir/main.cu.o"
+  "CMakeFiles/v2d.dir/cmake_device_link.o"
+  "v2d.pdb"
+  "v2d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/v2d.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
